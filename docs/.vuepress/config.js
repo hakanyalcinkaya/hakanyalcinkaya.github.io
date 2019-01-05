fdbc7fade09907e8a6e9eacdc7af3c3f5c4539d7
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-01-05 16:16:28 +0300

    config file change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,36 @@ function getMdFiles(path) {
   return files
 }
 
+let sideBar = [{
+  title: 'Sayfalar',
+  collapsable: false,
+  children: [
+      ['/', 'Ana Sayfa'],
+      '/hello-world',
+      '/pages/about_me',
+      '/pages/contact',
+    ]
+}]
+
+const sideBarNames = [
+  ['Python', 'Python'],
+  ['Django', 'Django'],
+  // ['JavaScript', 'JavaScript'],
+  ['YouTube', 'YouTube'],
+  ['Tools', 'Araçlar']
+]
+
+function getSideBars(names) {
+  let sideBarList = {}
+  names.forEach(function(item) {
+    sideBar.push({
+      title: item[1],
+      collapsable: false,
+      children: getMdFiles(item[0].toLowerCase())
+    })
+  })
+  return sideBar
+}
 
 module.exports = {
   plugins: [
@@ -29,50 +59,6 @@ module.exports = {
   	{ text: 'Hakkımda', link: '/pages/about_me.html'},
   	{ text: 'İletişim', link: '/pages/contact.html'},
   	],
-  	sidebar: [
-    {
-      title: 'Sayfalar',
-      collapsable: false,
-      children: [
-          ['/', 'Ana Sayfa'],
-          '/hello-world',
-          '/pages/about_me',
-          '/pages/contact',
-        ]
-      },
-      {
-        title: 'Python',
-        collapsable: false,
-        children: [
-          '/python/python-ve-kod-yazma-sanati',
-          '/python/statik-web-sitesi-olusturmak-icin-pelican',
-        ]
-      },
-      {
-        title: 'Django',
-        collapsable: false,
-        children: getMdFiles('django')
-      },
-      {
-        title: 'JavaScript',
-        collapsable: false,
-        children: []
-      },
-      {
-        title: 'YouTube',
-        collapsable: false,
-        children: getMdFiles('youtube')
-      },
-      {
-        title: 'Araçlar',
-        collapsable: false,
-        children: [
-          '/tools/flatuicolors',
-          '/tools/makefile-kullanin',
-          '/tools/markdown-ile-calismak',
-          '/tools/neden-mac-kullaniyorum',
-        ]
-      }
-  	]
+  	sidebar: getSideBars(sideBarNames)
   }
 }
